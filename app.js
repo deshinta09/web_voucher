97fbe62587d5f307a125cb0127c9e988ffae8b01
deshinta <deshintaaulia065@gmail.com>
2024-07-04 18:10:13 +0700

    feat: install cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 const router = require("./route/index");
 const errors = require("./middleware/errors");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
