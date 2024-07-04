ad1ef215b768845eb77d2fd1587395cd36432d33
deshinta <deshintaaulia065@gmail.com>
2024-07-04 16:03:49 +0700

    feat: add get categories

--- controller/controllerVoucher.js
+++ controller/controllerVoucher.js
@@ -23,6 +23,41 @@ class ControllerVoucher {
       next(error);
     }
   }
+
+  static async allCategories(req, res, next) {
+    try {
+      let categories = [];
+      let voucherId = [];
+      let checkCategories = [];
+      // mencari voucher yang telah di claim dari user yang sedang login
+      let voucherClaim = await Voucher_Claim.findAll(
+        { attributes: ["id_voucher"] },
+        { where: { id_user: req.user.id } }
+      );
+      // ambil id voucher yang telah di claim user yang sedang login
+      voucherClaim.forEach((el) => {
+        voucherId.push(el.id_voucher);
+      });
+      // ambil semua voucher kecuali yang sudah di claim user yang sedang login
+      let voucher = await Voucher.findAll({
+        where: { id: { [Op.not]: voucherId } },
+      });
+      // buat data yang menampilkan category dan jumlahnya
+      voucher.forEach((el) => {
+        let index = Infinity;
+        categories.forEach((n, i) => {
+          n.name === el.category ? (index = i) : "";
+        });
+        // console.log(categories[index], "<<< cate yg sama");
+        categories[index]
+          ? categories[index].num++
+          : categories.push({ name: el.category, num: 1 });
+      });
+      res.status(200).json(categories);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = ControllerVoucher;
--- route/voucher.js
+++ route/voucher.js
@@ -3,5 +3,6 @@ const ControllerVoucher = require("../controller/controllerVoucher");
 const router = express.Router();
 
 router.get("/voucher", ControllerVoucher.getAllVoucher);
+router.get("/categories", ControllerVoucher.allCategories);
 
 module.exports = router;
