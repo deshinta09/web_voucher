97c54a86b6574f25bf95113803be3fef7d1f8bce
deshinta <deshintaaulia065@gmail.com>
2024-07-10 07:58:39 +0700

    fix: get my categories

--- controller/controllerVoucher.js
+++ controller/controllerVoucher.js
@@ -64,17 +64,26 @@ class ControllerVoucher {
       let voucherClaim = await Voucher_Claim.findAll({
         where: { id_user: req.user.id },
       });
-      // console.log(voucherClaim[0].id_voucher, "<<< voucher yang udah di claim");
       // ambil id voucher yang telah di claim user yang sedang login
       voucherClaim.forEach((el) => {
         voucherId.push(el.id_voucher);
       });
-      // console.log(voucherId, "<<< di voucher");
       // ambil semua voucher kecuali yang sudah di claim user yang sedang login
       let voucher = await Voucher.findAll({
         where: { id: { [Op.in]: voucherId } },
       });
-      res.status(200).json(voucher);
+
+      voucher.forEach((el) => {
+        let index = Infinity;
+        categories.forEach((n, i) => {
+          n.category === el.category ? (index = i) : "";
+        });
+        categories[index]
+          ? categories[index].num++
+          : categories.push({ category: el.category, num: 1 });
+      });
+
+      res.status(200).json(categories);
     } catch (error) {
       next(error);
     }
